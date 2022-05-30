de32f04b5298d56af62c926d68b1dde1ae5739ba
LOCAL\maezaki <maezaki@guuun.co.jp>
2022-05-30 17:30:16 +0900

    木を追加 ・雲も追加したがイマイチなので削除 ・オブジェクトを発生させるスクリプトが重複してきたのでまとめる形でBGObjGeneratorというスクリプトを追加 　→いらないスクリプト（RailSet,GroundSet）を削除（bakフォルダへ一時移動）

--- Assets/Scripts/GraundObjController.cs
+++ Assets/Scripts/GraundObjController.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class GraundObjController : MonoBehaviour
 {
-    public float groundObjlSpeed;     //GroundObjの動くスピード
+    public float groundObjSpeed;     //GroundObjの動くスピード
     GameObject groundObj;
-    GroundSet groundSet;
+    //GroundSet groundSet;
+    BGObjGenerator bgObjGenerator;
 
     void Start()
     {
         groundObj = GameObject.FindGameObjectWithTag("Ground");
-        groundSet = groundObj.GetComponent<GroundSet>();
+        //groundSet = groundObj.GetComponent<GroundSet>();
+        bgObjGenerator = groundObj.GetComponent<BGObjGenerator>();
     }
 
     void Update()
     {
-        transform.position += new Vector3(-groundObjlSpeed * Time.deltaTime, 0f, 0f);
+        transform.position += new Vector3(-groundObjSpeed * Time.deltaTime, 0f, 0f);
         if (this.transform.position.x < -12.0f)
         {
-            groundSet.GroundCreate();
+            //groundSet.GroundCreate();
+            bgObjGenerator.GroundCreate();
             GroundObjBreak();
         }
     }
---
+++ Assets/Scripts/bak/GroundSet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSet : MonoBehaviour
+{
+    public GameObject groundPrefab;
+    float cleatePosX;
+
+    void Start()
+    {
+        cleatePosX = 30f;
+        GroundCreate();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GroundCreate()
+    {
+        GameObject childObject = Instantiate(groundPrefab, new Vector3(cleatePosX, 0f, -7f), Quaternion.identity) as GameObject;
+        childObject.transform.parent = this.transform;
+    }
+}
+
